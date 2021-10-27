79ebc0a238a7e7c441075151204057d0c48dfe15
Deepak M S <souparnikad40@gmail.com>
2021-10-27 21:11:57 +0530

    problem 2 finished

---
+++ Problem2/solution.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+//function for findig GCD
+
+int findGCD(int num1,int num2){
+    if(num1==0){
+        return num2;
+    }
+    if(num2==0){
+        return num1;
+    }
+
+    if(num1>num2){
+        return findGCD(num1%num2,num2);
+    }else {
+        return findGCD(num1,num2%num1);
+    }
+}
+
+int  main(){
+    int num1,num2,gcd;
+    //read the numbers 
+    cout<<"Enter the two numbers";
+    cin>>num1>>num2;
+
+    gcd = findGCD(num1,num2);
+
+    cout<<"GCD of "<<num1 <<" and "<<num2<<" is "<<gcd<<endl;
+
+    return 0;
+}
