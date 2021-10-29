12e9763a2dedf8bfe815cbecf1323125acec8f04
Deepak M S <souparnikad40@gmail.com>
2021-10-29 19:21:16 +0530

    problem1 statement and class added

---
+++ Problem.cpp
@@ -0,0 +1,25 @@
+/*
+Problem Statement : Write class to do all 4 arthematic functions. Depending on the user's choice,
+one of the operation is performed
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Arthematic{
+    public:
+        void addition(int a, int b){
+            cout<<"Addition Result"<<(a+b)<<endl;
+        }
+        void substraction(int a, int b){
+            cout<<"Addition Result"<<(a+b)<<endl;
+        }
+        void multiplication(int a, int b){
+            cout<<"Addition Result"<<(a+b)<<endl;
+        }
+        void division(int a, int b){
+            cout<<"Addition Result"<<(a+b)<<endl;
+        }
+};
+
