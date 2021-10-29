a757ac12eab11fe03d61f6780386758df8f6e2dc
Deepak M S <souparnikad40@gmail.com>
2021-10-29 19:30:35 +0530

    problem 1 finished

--- Problem.cpp
+++ Problem.cpp
@@ -10,16 +10,53 @@ using namespace std;
 class Arthematic{
     public:
         void addition(int a, int b){
-            cout<<"Addition Result"<<(a+b)<<endl;
+            cout<<"Addition Result = "<<(a+b)<<endl;
         }
         void substraction(int a, int b){
-            cout<<"Addition Result"<<(a+b)<<endl;
+            cout<<"Addition Result = "<<(a-b)<<endl;
         }
         void multiplication(int a, int b){
-            cout<<"Addition Result"<<(a+b)<<endl;
+            cout<<"Addition Result = "<<(a*b)<<endl;
         }
         void division(int a, int b){
-            cout<<"Addition Result"<<(a+b)<<endl;
+            cout<<"Addition Result = "<<(a/b)<<endl;
         }
 };
 
+int main(){
+    //object for Arthematic class
+    Arthematic obj;
+
+    int num1,num2,choice;
+
+    //read the number
+    cout<<"Enter two number : ";
+    cin>>num1>>num2;
+
+    //read choice
+    cout<<"select the Operator:"<<endl;
+    cout<<"\tEnter 1 for Addition\n\tEnter 2 for substraction"<<endl;
+    cout<<"\tEnter 3 for multiplication\n\tEnter 5 for division"<<endl;
+    cout<<"Enter Here:";
+    cin>>choice;
+
+    switch (choice)
+    {
+    case 1:
+        obj.addition(num1,num2);
+        break;
+    case 2:
+        obj.substraction(num1,num2);
+        break;
+    case 3:
+        obj.multiplication(num1,num2);
+        break;
+    case 4:
+        obj.division(num1,num2);
+        break;
+    
+    default:
+        cout<<"Invalid Entry"<<endl;
+        break;
+    }
+}
