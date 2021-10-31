c5de850c8496af2983aea6a3e8cd7046ec2fa288
Deepak M S <souparnikad40@gmail.com>
2021-10-31 17:59:02 +0530

    problem statements updated

--- Problem1/solution.cpp
+++ Problem1/solution.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 
-using namespace std;
+
 
 int main(){
     //variable declarations
     int num,i;
     bool isPrime=true;
     //read the number
-    cout<<"Enter the number";
-    cin>>num;
+    std::cout<<"Enter the number";
+    std::cin>>num;
 
     for(i=2;i<=num/2;i++){
         if(num % i==0){
@@ -18,9 +18,9 @@ int main(){
     }
 
     if(isPrime==false){
-        cout<<"Not a Prime Number"<<endl;
+        std::cout<<"Not a Prime Number"<<std::endl;
     }else{
-        cout<<"Prime number"<<endl;
+        std::cout<<"Prime number"<<std::endl;
     }
     
     return 0;
--- Problem2/solution.cpp
+++ Problem2/solution.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-using namespace std;
+
 
 //function for findig GCD
 
@@ -22,12 +22,12 @@ int findGCD(int num1,int num2){
 int  main(){
     int num1,num2,gcd;
     //read the numbers 
-    cout<<"Enter the two numbers";
-    cin>>num1>>num2;
+    std::cout<<"Enter the two numbers";
+    std::cin>>num1>>num2;
     //fucntion calliing
     gcd = findGCD(num1,num2);
 
-    cout<<"GCD of "<<num1 <<" and "<<num2<<" is "<<gcd<<endl;
+    std::cout<<"GCD of "<<num1 <<" and "<<num2<<" is "<<gcd<<std::endl;
 
     return 0;
 }
