73dcb01a7db2292da1e3fdc940089910f64b1437
Deepak M S <souparnikad40@gmail.com>
2021-11-29 11:15:05 +0530

    cycle2 problem3 finished

---
+++ Cycle2/Problem3/solution.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+//function definitions
+//call by value 
+void swap_by_value(int x,int y){
+    //swap
+    int temp =  x;
+    x=y;
+    y=temp;
+
+    cout<<"Value of formal are x = "<<x<<" and y = "<<y<<endl;
+
+}
+
+//call by address
+void swap_by_address(int *x , int *y){
+    int temp =  *x;
+    *x=*y;
+    *y=temp;
+
+    cout<<"Value of formal are x = "<<*x<<" and y = "<<*y<<endl;
+}
+
+//call by reference
+void swap_by_reference(int &x, int &y){
+    int temp =  x;
+    x=y;
+    y=temp;
+
+    cout<<"Value of formal are x = "<<x<<" and y = "<<y<<endl;
+
+}
+
+int main(){
+    int a, b;
+    //read the input
+    cout<<"Enter two integer value";
+    cin>>a>>b;
+
+    cout<<"\n\tCALL BY VALUE \n"<<endl;
+    cout<<"Before Passing ,Value of actual parmeters are a = "<<a<<" and b = "<<b<<endl;
+    swap_by_value(a,b);
+    cout<<"After Passing ,Value of actual parmeters are a = "<<a<<" and b = "<<b<<endl;
+
+    cout<<"\n\tCALL BY ADDRESS \n"<<endl;
+    cout<<"Before Passing ,Value of actual parmeters are a = "<<a<<" and b = "<<b<<endl;
+    swap_by_address(&a,&b);
+    cout<<"After Passing ,Value of actual parmeters are a = "<<a<<" and b = "<<b<<endl;
+
+
+    cout<<"\n\tCALL BY REFERENCE \n"<<endl;
+    cout<<"Before Passing ,Value of actual parmeters are a = "<<a<<" and b = "<<b<<endl;
+    swap_by_reference(a,b);
+    cout<<"After Passing ,Value of actual parmeters are a = "<<a<<" and b = "<<b<<endl;
+
+    return 0;
+}
