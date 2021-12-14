53d5aa10ed2db5b52febf9ebd7785979c7030511
DEEPAK M S <70992362+Deepak-coder80@users.noreply.github.com>
2021-12-14 10:31:02 +0530

    soultion updated

--- Cycle3/Problem1/solution.cpp
+++ Cycle3/Problem1/solution.cpp
@@ -1,10 +1,12 @@
-#include <iostream>
+/********************************************
+NAME : DEEPAK M S
+REG.NO. : 20220032
+********************************************/
 
+#include<iostream>
 using std::cout;
 using std::cin;
 using std::endl;
-
-
 //function for checking the year is leap year or not
 bool leapyear(int year){
      if (year % 400 == 0){
@@ -19,177 +21,73 @@ bool leapyear(int year){
     }
     return false;
 }
-//function for checking the validity of the date
-bool validator(int month,int day , int year){
-
-    if(year<0){
-        return false;
-    }
-    if(month<1||month>12){
-        return false;
-    }
-    if(day<1||day>31){
-        return false;
-    }
-
-    if(month==2){
-        if(leapyear(year)){
-            if(day>29){
-                return false;
-            }
-        }else{
-            if(day>28){
-                return false;
-            }
-        }
-    }
-
-    if (month ==4||month==6||month==9||month==11){
-        if(day>30){
-            return false;
-        }
-    }
-
-    return true;
-}
 class Date{
-    private:
-	int day;
-	int month;
-	int year;
-    public:
-	void setDate();
-	void display();
-	Date operator+(int d){
-		Date newDate;
-	
-		if(validator(month,day,year)){
-            //check whether month is 2
-            if(month == 2){
-                //check whether the year is leap year
-                if(leapyear(year)){
-                    if((day+d)>=29){
-                        if((day+day)-29==0){
-                            newDate.day=29;
-                            newDate.month=month;
-                            newDate.year=year;
-                        }else{
-                            newDate.day=(day+d)-29;
-                            newDate.month=month+1;
-                            newDate.year=year;
-                        }
-                        
-                    }else{
-                        newDate.day=day+d;
-                        newDate.month=month;
-                        newDate.year=year;
-                    }
-                }else{
-                    if((day+d)>=28){
-                        if((day+day)-28==0){
-                            newDate.day=28;
-                            newDate.month=month;
-                            newDate.year=year;
-                        }else{
-                            newDate.day=(day+d)-28;
-                            newDate.month=month+1;
-                            newDate.year=year;
-                        }
-                    }else{
-                        newDate.day=day+d;
-                        newDate.month=month;
-                        newDate.year=year;
-                    }
-                }
-            }else if(month ==4||month==6||month==9||month==11){
-                    if((day+d)>=30){
-                        if((day+day)-30==0){
-                            newDate.day=30;
-                            newDate.month=month;
-                            newDate.year=year;
-                        }else{
-                            newDate.day=(day+d)-30;
-                            newDate.month=month+1;
-                            newDate.year=year;
-                        }
-                    }else{
-                        newDate.day=day+d;
-                        newDate.month=month;
-                        newDate.year=year;
-                    }
-            }else if(month==12){
-                    if((day+d)>=31){
-                        if((day+day)-31==0){
-                            newDate.day=31;
-                            newDate.month=month;
-                            newDate.year=year;
-                        }else{
-                            newDate.day=(day+d)-31;
-                            newDate.month=1;
-                            newDate.year=year+1;
-                        }
-                    }else{
-                        newDate.day=day+d;
-                        newDate.month=month;
-                        newDate.year=year;
-                    }
-            }else{
-                    if((day+d)>=31){
-                        if((day+day)-31==0){
-                            newDate.day=31;
-                            newDate.month=month;
-                            newDate.year=year;
-                        }else{
-                            newDate.day=(day+d)-31;
-                            newDate.month=month+1;
-                            newDate.year=year;
-                        }
-                    }else{
-                        newDate.day=day+d;
-                        newDate.month=month;
-                        newDate.year=year;
-                    }
+	private:
+		int day,month,year;
+	public:
+		Date(int day,int month,int year){
+			this->day=day;
+			this->month=month;
+			this->year=year;
+		}
+		void operator +(int a){
+			
+			if(year>0 && 0<month<13&& 0<day<32){
+			day+=a;
+			do{
+            if(day>31 && (month==1||month==3||month==5||month==7||month==8||month==10||month==12)){
+				day=day-31;
+				if(month==12){
+					year=year+1;
+					month=1;
+				}else{
+					month=month+1;
+				}
+			}else if(day>30 && (month==4||month==6||month==9||month==11)){
+				day=day-30;
+				if(month==12){
+					year=year+1;
+					month=1;
+				}else{
+					month=month+1;
+				}
+			}else if((month==2)&&(day>28)){
+               			if(leapyear(year)){
+				  	day=day-29;
+                  			month=3;
 
-            }
-        }else{
-            cout<<"Enter a Valid Date!!"<<endl;
-        }
-		return newDate;
-	}
+               			}
+              			 else{
+				 	day=day-28;
+                 			 month=3;
+                   		}
+           	 }
+		}while(day>31);
+		
+		}else{
+			cout<<"Date is invalid"<<endl;
+			}		
+					
+		
 
+		}
+
+		void display(){
+			cout<<"The date is "<<day<<"/"<<month<<"/"<<year<<endl;
+		}
+		
 };
-void Date::setDate(){
-	cout<<"Enter day:";
-	cin>>day;
-	cout<<"Enter month:";
-	cin>>month;
-	cout<<"Enter year:";
-	cin>>year;
-}
-
-void Date::display(){
-    // if(validator(month,day,year)){
-	    cout<<"\nDate is "<<day<<"/"<<month<<"/"<<year<<endl;
-    // }
-}
-
 
 int main(){
-    int a;
-	//object creation
-	Date obj1,obj2;
-	obj1.setDate();
-	obj1.display();
+	int day,month,year;
+	int n;
+	cout<<"Enter date: ";
+	cin>>day>>month>>year;
+	Date date(day,month,year);
+	 cout<<"Enter the number of day want to add:";
+	cin>>n;
+	date+n;
+	date.display();
 	
-	
-    cout<<"Enter the number of day want to add:";
-    cin>>a;
-	obj2 = obj1 + a;
-
-    
-	obj2.display();
-
-	return 0;
+return 0;
 }
-
-
